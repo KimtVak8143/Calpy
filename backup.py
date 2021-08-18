a5de630b1b0f273c4a8c1998dd2504d022bb66bc
KimtVak8143 <mk8143@srmist.edu.in>
2021-08-18 18:30:09 +0530

    added test and other

--- backup.py
+++ backup.py
@@ -36,39 +36,18 @@ space = space.rjust(2, ' ')
 
 # code below is to print the calendar
 print(month_list[mm], yy)
-for i in days_list:
-    print(i, end=" ")
-print("")
-# print('Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa')
 
-if mm == 9 or mm == 4 or mm == 6 or mm == 11:
-    for i in range(31 + day):
+x = " "
+print(x.join(days_list))
+# print('SUN', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa')
 
-        if i <= day:
-            print(space, end=' ')
-        else:
-            print("{:02d}".format(i - day), end=' ')
-            if (i + 1) % 7 == 0:
-                print()
-elif mm == 2:
-    if yy % 4 == 0:
-        p = 30
+
+for i in range(31 + day):
+
+    if i <= day:
+        print(space, end='  ')
     else:
-        p = 29
+        print("{:02d}".format(i - day), end='  ')
+        if (i + 1) % 7 == 0:
+            print()
 
-    for i in range(p + day):
-        if i <= day:
-            print(space, end=' ')
-        else:
-            print("{:02d}".format(i - day), end=' ')
-            if (i + 1) % 7 == 0:
-                print()
-else:
-    for i in range(32 + day):
-
-        if i <= day:
-            print(space, end=' ')
-        else:
-            print("{:02d}".format(i - day), end=' ')
-            if (i + 1) % 7 == 0:
-                print()
